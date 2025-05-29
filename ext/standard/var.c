e4105d35937d0b2bbd2bc489ef076048cfe425cb
Michael Voříšek <mvorisek@mvorisek.cz>
2025-05-29 10:08:14 +0200

    Dump "unset" explicitly

--- ext/standard/var.c
+++ ext/standard/var.c
@@ -79,8 +79,8 @@ static void php_object_property_dump(zend_property_info *prop_info, zval *zv, ze
 	if (Z_TYPE_P(zv) == IS_UNDEF) {
 		ZEND_ASSERT(ZEND_TYPE_IS_SET(prop_info->type));
 		zend_string *type_str = zend_type_to_string(prop_info->type);
-		php_printf("%*cuninitialized(%s)\n",
-			level + 1, ' ', ZSTR_VAL(type_str));
+		php_printf("%*c%s(%s)\n",
+			level + 1, ' ', Z_PROP_FLAG_P(zv) & IS_PROP_UNINIT ? "unset" : "uninitialized", ZSTR_VAL(type_str));
 		zend_string_release(type_str);
 	} else {
 		php_var_dump(zv, level + 2);
@@ -283,8 +283,8 @@ static void zval_object_property_dump(zend_property_info *prop_info, zval *zv, z
 	}
 	if (prop_info && Z_TYPE_P(zv) == IS_UNDEF) {
 		zend_string *type_str = zend_type_to_string(prop_info->type);
-		php_printf("%*cuninitialized(%s)\n",
-			level + 1, ' ', ZSTR_VAL(type_str));
+		php_printf("%*c%s(%s)\n",
+			level + 1, ' ', Z_PROP_FLAG_P(zv) & IS_PROP_UNINIT ? "unset" : "uninitialized", ZSTR_VAL(type_str));
 		zend_string_release(type_str);
 	} else {
 		php_debug_zval_dump(zv, level + 2);
